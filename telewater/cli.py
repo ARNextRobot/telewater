5c77fa2aea1bcef7239f4463f51f84b51fc68457
ARNextRobot <83016893+ARNextRobot@users.noreply.github.com>
2021-05-22 13:36:30 -0300

    Update cli.py

--- telewater/cli.py
+++ telewater/cli.py
@@ -88,7 +88,7 @@ def main(
 ):
     """A telegram bot that applies watermark on images, gifs and videos.
 
-    For detailed docs read : https://github.com/aahnik/telewater
+    For detailed docs read : https://github.com/ARNextRobot/telewater
     """
 
     if FAKE:
